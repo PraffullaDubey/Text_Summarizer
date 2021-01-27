93bd9ea92f4bd1e179d13ef9fefabf906a0a8c84
Praffulla Dubey <praffullakrdubey@gmail.com>
2021-01-27 15:54:30 +0530

    fixed new issues

--- text_summarizer.py
+++ text_summarizer.py
@@ -21,8 +21,8 @@ def find_file_data(pathname):
     print(line,"\n") #printing the text present in file
     file1.close() #close the file
 
-#function to find summary using textrank
-def find_summary_textrank():
+#function to find summary using lexrank summarizer
+def find_summary_lexrank():
     p = PlaintextParser.from_file("testtext.txt", Tokenizer("english"))
     summarizer = LexRankSummarizer() #creating lexrank object
     summ = summarizer(p.document, 2) #Summarizing the document
@@ -30,7 +30,7 @@ def find_summary_textrank():
     for s in summ:
         print(s)
 
-#function to find summary using textrank        
+#function to find summary using stopwords method using lsa summarizer
 def find_summary_stopwords():
     p = PlaintextParser.from_file("testtext.txt", Tokenizer("english"))
     s_lsa = LsaSummarizer()
@@ -39,7 +39,7 @@ def find_summary_stopwords():
     for s in s_lsa(p.document,2):
         print(s)
 
-#function to find summary using textrank        
+#function to find summary using lsa summarizer      
 def find_summary_lsa():
     p = PlaintextParser.from_file("testtext.txt", Tokenizer("english"))
     sumlsa = LsaSummarizer()
@@ -47,7 +47,7 @@ def find_summary_lsa():
     for s in su:
         print(s)
 
-#function to find summary using textrank
+#function to find summary using luhn summarizer
 def find_summary_luhn():
     p = PlaintextParser.from_file("testtext.txt", Tokenizer("english"))
     s_luhn = LuhnSummarizer()
@@ -55,7 +55,9 @@ def find_summary_luhn():
     for s in su:
         print(s)
     
-
 pathname = "testtext.txt"
 find_file_data(pathname)
+find_summary_lexrank() #function call
+find_summary_stopwords() #function call
+find_summary_lsa() #function call
 find_summary_luhn() #function call
